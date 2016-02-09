2f1a2006b935dd83e6f26709e1f4ff9817aae81f
Pawanpreet Singh <pawanpreetsingh@icloud.com>
2016-02-09 16:11:40 +0530

    New Repo. Made Tempate file ad used in Index.php

---
+++ index.php
@@ -0,0 +1,550 @@
+<?php
+include('template.php');
+?>
+
+
+		<!-- Main Content -->
+		<div class="content-wrapper">
+			<!-- Hero Section -->
+			<section class="section-hero">
+				<!-- Main Slider -->
+				<div class="hero-slider">
+					<ul class="container custom-nav">
+						<li class="prev">
+							<i class="icon-arrowleft"></i>
+						</li>
+						<li class="next">
+							<i class="icon-arrowright"></i>
+						</li>
+					</ul>
+					<ul class="slides">
+						<li class="slide">
+							<div class="container">
+								<div class="slide-content">
+									<h1 class="heading slider-component">Experience delightful learning. Starting Now.
+<!-- 									 <a class="popup-holder" href="#"> -->
+<!-- 										<span class="typed">now </span> -->
+										<!-- 
+<span class="popup">
+											<span class="title">Best courses</span>
+											Crosswalk Website (new features: fully responsive, 1200px grid, SVG and Retina ready elements, page loading speed optimizations, updated payment system, etc.)
+										</span>
+ -->
+									</a></h1>
+
+									<a href="courses-list.html" class="slider-component second-row btn theme-btn-1">
+										<span class="button">Browse courses</span>
+									</a>
+								</div>
+							</div>
+						</li>
+
+						<li class="slide">
+							<div class="container">
+								<div class="slide-content">
+									<h1 class="heading slider-component">Choose something unique. Choose Us!
+									<!-- 
+ <a class="popup-holder" href="#">
+										<span class="typed"></span>
+										<span class="popup">
+									
+ -->		<!-- 
+<span class="title">Best courses</span>
+											Crosswalk Website (new features: fully responsive, 1200px grid, SVG and Retina ready elements, page loading speed optimizations, updated payment system, etc.)
+										</span>
+ -->
+<!-- 									</a> -->
+									</h1>
+
+									<a href="courses-list.html" class="slider-component second-row btn theme-btn-1">
+										<span class="button">Browse courses</span>
+									</a>
+								</div>
+							</div>
+						</li>
+
+						<li class="slide">
+							<div class="container">
+								<div class="slide-content">
+									<h1 class="heading slider-component">Build your future. Join us Today.
+									<!-- 
+<a class="popup-holder" href="#">
+										<span class="typed"></span>
+										<span class="popup">
+									
+ -->		<!-- 
+<span class="title">Best courses</span>
+											Crosswalk Website (new features: fully responsive, 1200px grid, SVG and Retina ready elements, page loading speed optimizations, updated payment system, etc.)
+										</span>
+ -->
+<!-- 									</a> -->
+									</h1>
+
+									<a href="courses-list.html" class="slider-component second-row btn theme-btn-1">
+										<span class="button">Browse courses</span>
+									</a>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</section>
+
+			<!-- Services Section -->
+			<section class="section-services">
+				<div class="container">
+					<div class="section-header">
+						<h1>Services</h1>
+					</div>
+
+					<div class="row">
+						<div class="col-xs-6 col-md-3 x-small">
+							<div class="simple-service">
+								<i class="icon icon-book-open"></i>
+								<h4 class="title">Placement Assistance</h4>
+								<p>Leverage the wide network of our experts spread across major accounting firms and MNCs to avail the best career opportunities</p>
+							</div>
+						</div>
+
+						<div class="col-xs-6 col-md-3 x-small">
+							<div class="simple-service">
+								<i class="icon icon-gears"></i>
+								<h4 class="title">Licensing Assistance</h4>
+								<p>Our team of licensed CPAs ensures that adequate guidance and help is provided to the students to get the CPA license and attain the most reputed CPA designation</p>
+							</div>
+						</div>
+
+						<div class="col-xs-6 col-md-3 x-small">
+							<div class="simple-service">
+								<i class="icon icon-calendar"></i>
+								<h4 class="title">Award Program</h4>
+								<p>Stand out among your fellow CPA aspirants to get your hands on cash rewards intended for exceptional performance</p>
+							</div>
+						</div>
+
+						<div class="col-xs-6 col-md-3 x-small">
+							<div class="simple-service">
+								<i class="icon icon-flag"></i>
+								<h4 class="title">Supplementary Material</h4>
+								<p>Prepare on the go with 50 hours of audio files and more than 2000 flash cards made available to you as part of the enrollment</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="services-description">
+						<div class="row">
+							<div class="col-md-6">
+								<p class="highlighted-start">The Certified Public Accountant (CPA) is the highest accounting credential in the U.S. It is a symbol of trust and professionalism in the world of business. To earn the CPA qualification, one has to pass an exam administered by the American Institute of Certified Public Accountant (AICPA).</p>
+
+								<p>Crosswalk specializes in preparing students for the CPA exam and offers a complete CPA review program. We are focused on creating value and providing highest quality and exam oriented training to our students with:</p>
+
+								<ul class="list-services">
+									<li>Proprietary content and innovative teaching methodology</li>
+									<li>Designed to ensure a delightful experience followed by</li>
+									<li>End to end support till you reach the doorstep of success</li>
+								</ul>
+							</div>
+
+							<div class="col-md-6 col-lg-offset-1 col-lg-5">
+								<div class="showcase-box">
+									<div class="box-wrapper" data-parallax-bg="img/showcase-bg.jpg">
+										<div class="box-img"><span></span></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- Courses Section -->
+			<section class="section-courses-preview">
+				<div class="container">
+					<div class="section-header">
+						<h1>Courses</h1>
+					</div>
+				</div>
+
+				<div class="row row-fit">
+					<div class="col-md-6">
+						<div class="course-preview design">
+							<div class="course-wrapper">
+								<span class="bg"></span>
+								<a href="courses-grid.html" class="description">
+									<h4 class="title">Auditing and Attestation</h4>
+									<p>Auditing procedures, generally accepted auditing standards and other standards related to attest engagements.</p>
+								</a>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6">
+						<div class="course-preview business">
+							<div class="course-wrapper">
+								<span class="bg"></span>
+								<a href="courses-list.html" class="description">
+									<h4 class="title">Regulation</h4>
+									<p>Federal taxation, professional and legal responsibilities, ethics, and business law, and the skills required to apply that knowledge.</p>
+								</a>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6">
+						<div class="course-preview health">
+							<div class="course-wrapper">
+								<span class="bg"></span>
+								<a href="courses-grid.html" class="description">
+									<h4 class="title">Financial Accounting and Reporting</h4>
+									<p>Accounting principles for business enterprises, not-for- profit organizations, and governmental entities.</p>
+								</a>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6">
+						<div class="course-preview hospitality">
+							<div class="course-wrapper">
+								<span class="bg"></span>
+								<a href="courses-list.html" class="description">
+									<h4 class="title">Business Environment and Concepts</h4>
+									<p>that candidates should know in order to understand the business reasons for and accounting implications of transactions.</p>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="btn-wrapper">
+					<a href="courses-list.html" class="btn theme-btn-2" style="margin-top:20px;">Register for training</a>
+				</div>
+			</section>
+
+			<!-- Testimonials Section hiding it for now 
+
+			<section class="section-testimonial">
+				<div class="container">
+					<div class="section-header">
+						<h1>Testimonials</h1>
+					</div>
+				</div>
+
+				<ul class="testimonials-list">
+					<li class="slide">
+						<div class="container">
+							<div class="testimonial">
+								<div class="testimonial-wrapper">
+									<p class="message">"Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedid quo minus id quos maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus."</p>
+									<p class="author">Scott Eastwood, <span class="title">Student</span></p>
+								</div>
+							</div>
+						</div>
+					</li>
+
+					<li class="slide">
+						<div class="container">
+							<div class="testimonial">
+								<div class="testimonial-wrapper">
+									<p class="message">"Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedid quo minus id quos maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus."</p>
+									<p class="author">Scott Eastwood, <span class="title">Student</span></p>
+								</div>
+							</div>
+						</div>
+					</li>
+
+					<li class="slide">
+						<div class="container">
+							<div class="testimonial">
+								<div class="testimonial-wrapper">
+									<p class="message">"Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedid quo minus id quos maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus."</p>
+									<p class="author">Scott Eastwood, <span class="title">Student</span></p>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</section>
+		-->
+
+			<!-- Courses Section -->
+			<!-- commenting out popular courses section
+			<section class="section-courses no-padding">
+				<div class="container">
+					<div class="section-header">
+						<h1>Popular Courses</h1>
+					</div>
+
+					<div class="projects">
+						<div class="main-loader">
+							<div class="loader-front">
+								<img src="img/loader-front.png" alt="loader front" />
+							</div>
+						</div>
+
+						<div class="ajax-target">
+							<div class="row">
+								<div class="col-md-4 col-sm-6">
+									<div class="single-course grid-course">
+										<div class="course-thumbnail">
+											<a href="course.html" class="cover-info">
+												<div class="text">
+													<h4>Don't miss this course</h4>
+													<p>These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able.</p>
+												</div>
+											</a>
+
+											<img src="img/grid-course-1.jpg" alt="course thumbnail" />
+										</div>
+
+										<div class="course-details">
+											<h3 class="title">
+												<a href="course.html">Best Measurement course in the world</a>
+											</h3>
+
+											<ul class="course-meta">
+												<li><span>Instructor:</span> John Isner</li>
+												<li><span>Type:</span> Offline</li>
+												<li><span>Category:</span> Art</li>
+											</ul>
+
+											<div class="course-action">
+												<a href="#" class="btn theme-btn-1 register-btn">
+													<span class="button">Register</span>
+												</a>
+
+												<span class="price">$39</span>
+											</div>
+										</div>
+									</div>	
+								</div>
+
+								<div class="col-md-4 col-sm-6">
+									<div class="single-course grid-course">
+										<div class="course-thumbnail">
+											<a href="course-online.html" class="cover-info">
+												<div class="text">
+													<h4>Don't miss this course</h4>
+													<p>These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able.</p>
+												</div>
+											</a>
+
+											<img src="img/grid-course-2.jpg" alt="course thumbnail" />
+										</div>
+
+										<div class="course-details">
+											<h3 class="title">
+												<a href="course.html">Culinary courses</a>
+											</h3>
+
+											<ul class="course-meta">
+												<li><span>Instructor:</span> Zack Thoumb</li>
+												<li><span>Type:</span> Online</li>
+												<li><span>Category:</span> Culinary</li>
+											</ul>
+
+											<div class="course-action">
+												<a href="#" class="btn theme-btn-1 register-btn">
+													<span class="button">Register</span>
+												</a>
+
+												<span class="price">$99</span>
+											</div>
+										</div>
+									</div>	
+								</div>
+
+								<div class="col-md-4 col-sm-6">
+									<div class="single-course grid-course">
+										<div class="course-thumbnail">
+											<a href="course-online.html" class="cover-info">
+												<div class="text">
+													<h4>Don't miss this course</h4>
+													<p>These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able.</p>
+												</div>
+											</a>
+
+											<img src="img/grid-course-3.jpg" alt="course thumbnail" />
+										</div>
+
+										<div class="course-details">
+											<h3 class="title">
+												<a href="course.html">technology for kids</a>
+											</h3>
+
+											<ul class="course-meta">
+												<li><span>Instructor:</span> John Isner</li>
+												<li><span>Type:</span> Online</li>
+												<li><span>Category:</span> Technology</li>
+											</ul>
+
+											<div class="course-action">
+												<a href="#" class="btn theme-btn-1 enroll-btn">
+													<span class="button">Enroll</span>
+												</a>
+
+												<span class="price">$39</span>
+
+												<span class="save-btn icon-heart5"><span class="popup">saved</span></span>
+											</div>
+										</div>
+									</div>	
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			-->
+			<!-- Links Box hidden for now 
+			<div class="links-wrapper">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-6 col-md-3">
+							<div class="links-box">
+								<h3 class="caption">Technology</h3>
+
+								<ul class="links">
+									<li><a href="course.html">Straight A Fund</a></li>
+									<li><a href="course.html">Financial Information Data</a></li>
+									<li><a href="course.html">Performance Data</a></li>
+									<li><a href="course.html">Race to the Top</a></li>
+									<li><a href="course.html">School Safety Plans</a></li>
+								</ul>								
+							</div>
+						</div>
+
+						<div class="col-sm-6 col-md-3">
+							<div class="links-box">
+								<h3 class="caption">Culinary</h3>
+
+								<ul class="links">
+									<li><a href="course-online.html">Straight A Fund</a></li>
+									<li><a href="course-online.html">Financial Information Data</a></li>
+									<li><a href="course-online.html">Performance Data</a></li>
+									<li><a href="course-online.html">Race to the Top</a></li>
+									<li><a href="course-online.html">School Safety Plans</a></li>
+								</ul>								
+							</div>
+						</div>
+
+						<div class="col-sm-6 col-md-3">
+							<div class="links-box">
+								<h3 class="caption">Education</h3>
+
+								<ul class="links">
+									<li><a href="course-online.html">Straight A Fund</a></li>
+									<li><a href="course-online.html">Financial Information Data</a></li>
+									<li><a href="course-online.html">Performance Data</a></li>
+									<li><a href="course-online.html">Race to the Top</a></li>
+								</ul>								
+							</div>
+						</div>
+
+						<div class="col-sm-6 col-md-3">
+							<div class="links-box">
+								<h3 class="caption">Sports</h3>
+
+								<ul class="links">
+									<li><a href="course.html">Straight A Fund</a></li>
+									<li><a href="course.html">Financial Information Data</a></li>
+									<li><a href="course.html">Performance Data</a></li>
+									<li><a href="course.html">Race to the Top</a></li>
+									<li><a href="course.html">School Safety Plans</a></li>
+								</ul>								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			Unhide from here -->
+
+			<!-- Call to Action Box -->
+			<div class="call-to-action-box" data-parallax-bg="img/call-to-action-bg.jpg">
+				<div class="box-img"><span></span></div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-5">
+							<h2>Drop us an email Today</h2>
+						</div>
+					
+					<div class="col-md-3">
+							<input type="text" placeholder="Email Address" style="width:100%; margin-top:10px; height:35px;  border-radius:3px; padding:5px"/>
+						</div>
+					
+					
+						<div class="col-md-4">
+							<div class="button-wrapper">
+								<a href="contact.html" class="btn theme-btn-3">Get in touch</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<footer class="fixed">
+			<div class="container">
+				<div class="footer-wrapper">
+					<img src="img/director.png" alt="footer brand" class="footer-brand" />
+
+					<ul class="social-block">
+						<li><a href="https://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="https://dribbble.com/"><i class="fa fa-dribbble"></i></a></li>
+						<li><a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="https://www.pinterest.com/"><i class="fa fa-pinterest"></i></a></li>
+						<li><a href="https://www.facebook.com/"><i class="fa fa-instagram"></i></a></li>
+						<li><a href="https://www.linkedin.com"><i class="fa fa-linkedin"></i></a></li>
+					</ul>
+
+					<div class="main-area">
+						<div class="menu">
+							<ul>
+								<li><a href="index.html">Home</a></li>
+								<li><a href="courses-grid.html">Courses</a></li>
+								<li><a href="pricing.html">Pricing</a></li>
+								<li><a href="course.html">Single Course</a></li>
+								<li><a href="contact.html">Contact</a></li>
+							</ul>
+						</div>
+
+						<div class="row">
+							<div class="col-md-4">
+								<div class="footer-widget widget_info">
+									<p><span>Vipul Mittal</span><br />Certified CPA</p>
+								</div>
+							</div>
+
+							<div class="col-md-4">
+								<div class="footer-widget widget_contact">
+									<p>Crosswalk Professional Education Pvt Ltd<br />+91 9540679259 |<br /><a href="#">info@crosswalkcpa.com</a></p>
+								</div>
+							</div>
+
+							<!-- 
+<div class="col-md-4">
+								<div class="footer-widget widget_tagcloud">
+									<div class="tagcloud">
+										<a href="#">Education</a>
+										<a href="#">Courses</a>
+										<a href="#">CPA</a>
+										<a href="#">FAR</a>
+										<a href="#">AUD</a>
+										<a href="#">REG</a>
+										<a href="#">Instruction</a>
+										<a href="#">Skills</a>
+										<a href="#">BEC</a>
+									</div>
+								</div>
+							</div>
+ -->
+						</div>
+					</div>
+
+					<div class="copyrights">
+						<p>Copyright 2015. Crosswalk Professional Education Pvt Ltd</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+		
+		
---
+++ template.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <title>Crosswalk CPA Review</title>
+    <meta name="description" content="Here goes description" />
+    <meta name="author" content="author name" />
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon" />
+
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/icomoon.css" />
+    <link rel="stylesheet" href="css/screen.css" />
+</head>
+<body id="front-page" data-smooth-scroll="on">
+	<!-- 
+<form class="global-search-form">
+		<div class="container">
+			<input type="text" name="search-input" class="search-input js-input" placeholder="Type here ..." />
+		</div>
+	</form>
+ -->
+
+	<!-- Page Wrapper -->
+	<div id="page">
+		<!-- Register Poup -->
+		<div class="register-popup">
+			<div class="popup-wrapper">
+				<span class="close-popup-btn icon-cross"></span>
+
+				<form id="register-form">
+					<div class="section-header">
+						<h1>Register</h1>
+					</div>
+
+					<label>
+						<span>First Name *</span>
+						<input type="text" class="js-input" name="register-first-name" />
+					</label>
+
+					<label>
+						<span>Last Name *</span>
+						<input type="text" class="js-input" name="register-last-name" />
+					</label>
+
+					<label>
+						<span>E-mail address *</span>
+						<input type="text" class="js-input" name="register-email" />
+					</label>
+
+					<label>
+						<span>Password *</span>
+						<input type="password" class="js-input" name="register-password" />
+					</label>
+
+					<label>
+						<span>Confirm Password *</span>
+						<input type="password" class="js-input" name="register-password" />
+					</label>
+
+					<div class="btn-wrapper">
+						<button class="btn theme-btn-3">Register</button>
+					</div>
+
+					<div class="section-header small">
+						<h1><span>or</span></h1>
+					</div>
+
+					<div class="social-buttons">
+						<a href="#" class="facebook">facebook</a>
+						<a href="#" class="google-plus">google+</a>
+					</div>
+
+					<p><a href="#" class="forgot-password">Forgot password?</a></p>
+					<p>Have an account already? <a href="#" class="login-btn">Login here</a></p>
+				</form>
+
+				<form id="login-form">
+					<div class="section-header">
+						<h1>Login</h1>
+					</div>
+
+					<label>
+						<span>E-mail address *</span>
+						<input type="text" class="js-input" name="login-email" />
+					</label>
+
+					<label>
+						<span>Password *</span>
+						<input type="password" class="js-input" name="login-password" />
+					</label>
+
+					<div class="btn-wrapper">
+						<button class="btn theme-btn-3">Login</button>
+					</div>
+
+					<div class="section-header small">
+						<h1><span>or</span></h1>
+					</div>
+
+					<div class="social-buttons">
+						<a href="#" class="facebook">facebook</a>
+						<a href="#" class="google-plus">google+</a>
+					</div>
+					
+					<p><a href="#" class="forgot-password">Forgot password?</a></p>
+					<p>Don't have an account? <a href="#" class="register-btn">Register here</a></p>
+				</form>
+			</div>
+		</div>
+		
+		<!-- Header -->
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-4 col-sm-2">
+						<a class="brand" href="index.html">
+							<img src="img/identity.jpg" alt="identity" />
+						</a>
+					</div>
+
+					<div class="col-xs-8 col-sm-10">
+						<div class="action-bar">
+							<ul class="social-block">
+								<li><a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://twitter.com/"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="https://www.pinterest.com/"><i class="fa fa-pinterest"></i></a></li>
+								<li><a href="https://dribbble.com/"><i class="fa fa-dribbble"></i></a></li>
+							</ul>
+
+							<span class="search-box-toggle no-select">
+								<i class="icon fa fa-search"></i>
+							</span>
+
+							<span class="menu-toggle no-select">Menu
+								<span class="hamburger">
+									<span class="menui top-menu"></span>
+									<span class="menui mid-menu"></span>
+									<span class="menui bottom-menu"></span>
+								</span>
+							</span>
+
+							<div class="shopping-cart">
+								<span class="shopping-cart-toggle icon-carticon">
+									<span class="cart-items-total">2</span>
+								</span>
+
+								<div class="cart">
+									<ul>
+										<li class="cart-course">
+											<a href="#" class="remove-from-cart"><i class="icon-cross"></i></a>
+											<a class="cover" href="course.html">
+												<img src="img/cart-course.jpg" alt="cart course cover" />
+											</a>
+											<p class="title">
+												<a href="course.html">Technology for kids</a>
+											</p>
+											<span class="price">$59</span>
+										</li>
+
+										<li class="cart-course">
+											<a href="#" class="remove-from-cart"><i class="icon-cross"></i></a>
+											<a class="cover" href="course.html">
+												<img src="img/cart-course-2.jpg" alt="cart course cover" />
+											</a>
+											<p class="title">
+												<a href="course.html">Best measurement course</a>
+											</p>
+											<span class="price">$39</span>
+										</li>
+									</ul>
+
+									<div class="cart-action">
+										<a href="cart.html" class="cart-btn">View cart</a>
+									</div>
+								</div>
+							</div>
+<!--  href="myaccount.html" -->
+							<a class="login" STYLE="COLOR:WHITE"> REGISTER/LOGIN
+							<!--<span class="icon icon-MyAccount"></span><span class="popup">My account</span>--></a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<nav>
+				<ul>
+					<li class="current-menu-item"><a href="index.html">Home</a></li>
+					<li>
+						<a href="courses-grid.html">Courses</a>
+						<ul>
+							<!--<li><a href="courses-grid.html">Grid overlay</a></li>-->
+							<li><a href="courses-list.html">Subjects</a></li>
+							<li><a href="pricing.html">Pricing</a></li>
+							<li><a href="course.html">Demo</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">About the exam</a>
+						<ul>
+							<li><a href="members.html">What is CPA?</a></li>
+							<!-- <li><a href="pricing.html">Pricing</a></li> -->
+							<li><a href="404.html">State CPA requirements</a></li>
+							<li><a href="cart.html">Why Crosswalk?</a></li>
+							<!--<li><a href="myaccount.html">My Account</a></li>-->
+						</ul>
+					</li>
+					<li>
+						<a href="blog.html">Blog</a>
+						<!--
+						<ul>
+							<li><a href="blog.html">Blog</a></li>
+							<li><a href="blog-sidebar.html">Blog with sidebar</a></li>
+							<li><a href="blogpost.html">Blog post</a></li>
+							<li><a href="blogpost-sidebar.html">Blog post with sidebar</a></li>
+						</ul>
+						-->
+					</li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+<!-- Footer -->
+		
+	</div>
+
+	<!-- Scripts -->
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery-ui.js"></script>
+	<script src="js/lightbox.js"></script>
+	<script src="js/velocity.js"></script>
+	<script src="js/modernizr.js"></script>
+	<script src="js/smooth-scroll.js"></script>
+	<script src="js/bxslider.js"></script>
+	<script src="js/options.js"></script>
+</body>
+</html>
